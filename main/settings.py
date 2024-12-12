ecf439c76e2df11f86e3ba09e2b95bedfba499b7
sungkam718 <sungkam718@gmail.com>
2024-12-12 15:27:12 +1100

    updated 95

--- main/settings.py
+++ main/settings.py
@@ -182,7 +182,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STORAGES = {
     "default": {
