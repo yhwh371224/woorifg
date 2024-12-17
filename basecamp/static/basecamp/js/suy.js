dbaded790ca55ae168dd9c0249f51b280e6a6686
sungkam718 <sungkam718@gmail.com>
2024-12-17 16:08:37 +1100

    updated 148

--- basecamp/static/basecamp/js/suy.js
+++ basecamp/static/basecamp/js/suy.js
@@ -1,19 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
     const images = [
-        "{% static 'basecamp/assets/img/suy7.webp' %}",
-        "{% static 'basecamp/assets/img/suy8.webp' %}",
-        "{% static 'basecamp/assets/img/suy5.webp' %}"
+        "/static/basecamp/assets/img/suy7.webp",  
+        "/static/basecamp/assets/img/suy8.webp",
+        "/static/basecamp/assets/img/suy5.webp"
     ];
 
-    let currentIndex = 0; 
+    let currentIndex = 0;
 
     function changeBackgroundImage() {
         const header = document.querySelector('header.masthead');
         if (header) {
-            header.style.backgroundImage = `url(${images[currentIndex]})`; 
+            header.style.backgroundImage = `url(${images[currentIndex]})`;
             currentIndex = (currentIndex + 1) % images.length;
         }
-    }      
+    }
 
     setInterval(changeBackgroundImage, 3000);
 });
