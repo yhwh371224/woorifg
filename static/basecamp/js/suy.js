3554d6a9fe280284aa097c7a0ec1cf7c4feab79f
sungkam718 <sungkam718@gmail.com>
2024-12-17 15:58:17 +1100

    updated 146

--- static/basecamp/js/suy.js
+++ static/basecamp/js/suy.js
@@ -1,18 +1,19 @@
-const images = [
-"{% static 'basecamp/assets/img/suy8.webp' %}",
-"{% static 'basecamp/assets/img/suy7.webp' %}",
-"{% static 'basecamp/assets/img/suy5.webp' %}" 
-];
+document.addEventListener("DOMContentLoaded", function() {
+    const images = [
+        "{% static 'basecamp/assets/img/suy7.webp' %}",
+        "{% static 'basecamp/assets/img/suy8.webp' %}",
+        "{% static 'basecamp/assets/img/suy5.webp' %}"
+    ];
 
-let currentIndex = 0; 
+    let currentIndex = 0; 
 
-function changeBackgroundImage() {
-const header = document.querySelector('header.masthead');          
+    function changeBackgroundImage() {
+        const header = document.querySelector('header.masthead');
+        if (header) {
+            header.style.backgroundImage = `url(${images[currentIndex]})`; 
+            currentIndex = (currentIndex + 1) % images.length;
+        }
+    }      
 
-header.style.backgroundImage = `url(${images[currentIndex]})`;          
-
-currentIndex = (currentIndex + 1) % images.length;
-}      
-
-setInterval(changeBackgroundImage, 3000);
-
+    setInterval(changeBackgroundImage, 3000);
+});
