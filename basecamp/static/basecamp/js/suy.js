16784cc2bd1571a2d06e63b1da190ae4e980e689
sungkam718 <sungkam718@gmail.com>
2024-12-17 15:06:51 +1100

    updated 141

--- basecamp/static/basecamp/js/suy.js
+++ basecamp/static/basecamp/js/suy.js
@@ -16,29 +16,15 @@ currentIndex = (currentIndex + 1) % images.length;
 
 setInterval(changeBackgroundImage, 3000);
 
-// Function to hide the notice box and open the link
-function hideNoticeBox(event, link, target) {
-    event.preventDefault(); // Prevent default link behavior
-    const noticeBox = document.querySelector('.notice-box-link');
-    if (noticeBox) {
-        noticeBox.style.display = 'none'; // Hide the notice box
-    }
-    // Open the link in the appropriate target
-    if (target === "_blank") {
-        window.open(link, target);
-    } else {
-        window.location.href = link; // Navigate to the link in the same window
-    }
-}
-// Function to hide the notice box when the close button is clicked
-function closeNoticeBox(event) {
-    event.preventDefault(); // Prevent the default link behavior temporarily
-        const noticeBox = document.querySelector('.notice-box-link');
-        if (noticeBox) {
-            noticeBox.style.display = 'none'; // Hide the notice box
+// Bootstrap 5 JavaScript로 설정
+var dropdowns = document.querySelectorAll('.dropdown-toggle');
+
+dropdowns.forEach(function(dropdown) {
+    dropdown.addEventListener('click', function (event) {
+        var menu = this.nextElementSibling;
+        if (window.innerWidth <= 767.95) {
+            event.preventDefault(); // 메뉴가 바로 사라지지 않도록 함
+            menu.classList.toggle('show'); // 메뉴 열기
         }
-        // Allow the link to open after hiding the notice box
-        setTimeout(() => {
-            window.open(event.currentTarget.href, event.currentTarget.target);
-        }, 0);
-    }
+    });
+});
