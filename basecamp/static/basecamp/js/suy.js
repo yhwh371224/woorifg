5705a5132de36148d26065cbcb491b631cda4a92
sungkam718 <sungkam718@gmail.com>
2024-12-17 15:30:23 +1100

    updated 145

--- basecamp/static/basecamp/js/suy.js
+++ basecamp/static/basecamp/js/suy.js
@@ -16,13 +16,3 @@ currentIndex = (currentIndex + 1) % images.length;
 
 setInterval(changeBackgroundImage, 3000);
 
-document.addEventListener('DOMContentLoaded', function() {
-    // 모바일 환경에서만 적용
-    if (window.innerWidth <= 767.95) {
-        document.querySelectorAll('.dropdown-item').forEach(function(item) {
-            item.addEventListener('click', function(e) {
-                e.stopPropagation(); // 메뉴가 닫히지 않도록 방지
-            });
-        });
-    }
-});
