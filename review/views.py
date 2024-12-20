c4ebc4ea2693c27d3eb52a978576d11efc2659ac
sungkam718 <sungkam718@gmail.com>
2024-12-20 12:04:26 +1100

    updated 215

--- review/views.py
+++ review/views.py
@@ -21,6 +21,8 @@ class PostList(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(PostList, self).get_context_data(**kwargs)
         context['post_count'] = Post.objects.all().count()
+        context['category_list'] = Category.objects.all()
+        context['posts_without_category'] = Category.objects.filter(category=None).count()
 
         user_name = None
         if self.request.user.is_authenticated:
@@ -226,7 +228,7 @@ class PostListByCategory(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(type(self), self).get_context_data(**kwargs)
         context['category_list'] = Category.objects.all()
-        context['reviews_without_category'] = Post.objects.filter(category=None).count()
+        context['posts_without_category'] = Post.objects.filter(category=None).count()
 
         slug = self.kwargs['slug']
 
