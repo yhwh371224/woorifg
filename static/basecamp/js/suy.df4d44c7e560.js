0414bb25b1e739d3e7a71ef0a7183df5cbbfbb02
sungkam718 <sungkam718@gmail.com>
2024-12-21 16:45:16 +1100

    updated 232

---
+++ static/basecamp/js/suy.df4d44c7e560.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const masthead = document.querySelector("header.masthead");
+    const images = [
+        "/static/basecamp/assets/img/suy7.webp",
+        "/static/basecamp/assets/img/suy8.webp",
+        "/static/basecamp/assets/img/suy5.webp",
+    ];
+    let currentIndex = 0;
+
+    function changeBackground() {
+        masthead.style.backgroundImage = `url('${images[currentIndex]}')`;
+        currentIndex = (currentIndex + 1) % images.length;
+    }
+
+    // 5초마다 배경 변경
+    setInterval(changeBackground, 5000);
+});
