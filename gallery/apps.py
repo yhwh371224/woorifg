23cdf97e49d7475f43fa11398c998c5f12645ddd
sungkam718 <sungkam718@gmail.com>
2024-12-27 15:16:52 +1100

    updated 259

--- gallery/apps.py
+++ gallery/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class BlogConfig(AppConfig):
+class GalleryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gallery'
 
