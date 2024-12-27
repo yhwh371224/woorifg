b3b5aa3304145386fa9d215d3075121bf82c4ad6
sungkam718 <sungkam718@gmail.com>
2024-12-27 15:01:05 +1100

    updated 257

--- gallery/signals.py
+++ gallery/signals.py
@@ -4,13 +4,15 @@ from django.dispatch import receiver
 from gallery.models import Gallery
 from .tasks import convert_webp
 
-
 @receiver(post_save, sender=Gallery)
 def convert_webp_after_save(sender, instance, created, **kwargs):
 
     if created and instance.head_image:
         img_path = instance.head_image.path
-        if img_path.lower().endswith('.webp'):
+
+        if not img_path.lower().endswith('.webp'):
+            print(f"Triggering conversion for image: {img_path}")
+
+            convert_webp.apply_async(args=[instance.id])
+        else:
             print(f"{img_path} is already in WebP format, no conversion needed.")
-            return
-        convert_webp.apply_async(args=[instance.id])
--- gallery/tasks.py
+++ gallery/tasks.py
@@ -1,8 +1,8 @@
+# gallery/tasks.py
 from celery import shared_task
 from PIL import Image, ImageOps
 import os
 from django.conf import settings
-
 
 @shared_task
 def convert_webp(gallery_id):
@@ -15,25 +15,23 @@ def convert_webp(gallery_id):
         if img_path.lower().endswith(('jpg', 'jpeg', 'png')) and os.path.exists(img_path):
             img = Image.open(img_path)
 
-            # EXIF 회전 정보 적용
             img = ImageOps.exif_transpose(img)
 
             webp_path = os.path.splitext(img_path)[0] + '.webp'
-
             img.save(webp_path, 'WEBP', quality=80)
 
-            # 기존 경로에 맞게 저장될 수 있도록 수정
             new_path = os.path.join(settings.MEDIA_ROOT, 'gallery', str(gallery.date.year), 
                                     str(gallery.date.month), str(gallery.date.day), 
                                     os.path.basename(webp_path))
             os.makedirs(os.path.dirname(new_path), exist_ok=True)
 
-            gallery.head_image.name = os.path.relpath(webp_path, settings.MEDIA_ROOT)
+            gallery.head_image.name = os.path.relpath(new_path, settings.MEDIA_ROOT)
             gallery.save()
 
             os.remove(img_path)
 
-            print(f"Converted {img_path} to {webp_path}")
+            print(f"Converted {img_path} to {new_path}")
     except Exception as e:
         print(f"Error converting image {gallery_id}: {e}")
+
 
