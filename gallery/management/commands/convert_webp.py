dafdc6ad1da76c979402bee12abb07c95e66ad4c
sungkam718 <sungkam718@gmail.com>
2024-12-27 20:34:21 +1100

    updated 237

--- gallery/management/commands/convert_webp.py
+++ gallery/management/commands/convert_webp.py
@@ -1,5 +1,5 @@
 import os
-from PIL import Image
+from PIL import Image, ImageOps
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from gallery.models import Gallery
@@ -21,25 +21,30 @@ class Command(BaseCommand):
                         img_dir, img_filename = os.path.split(img_path)  # 디렉터리와 파일명 분리
                         img_name, img_ext = os.path.splitext(img_filename)  # 파일명과 확장자 분리
 
+                        # EXIF 회전 정보 적용
+                        img = ImageOps.exif_transpose(img)
+
                         # WebP 저장 경로 설정 (기존 날짜별 구조 유지)
                         webp_path = os.path.join(img_dir, f"{img_name}.webp")
 
-                        if img.format != 'WEBP':
-                            # WebP로 변환
+                        if img_ext.lower() not in ['.webp']:
+                            # WebP로 변환 및 저장
                             img.save(webp_path, 'WEBP', quality=80)
 
-                            # Django 모델에 새로운 이미지 경로 저장
-                            gallery.head_image.name = os.path.relpath(webp_path, settings.MEDIA_ROOT)
-                            gallery.save()
+                            if os.path.exists(webp_path):
+                                # Django 모델에 새로운 이미지 경로 저장
+                                gallery.head_image.name = os.path.relpath(webp_path, settings.MEDIA_ROOT)
+                                gallery.save()
 
-                            # 원본 이미지 삭제
-                            os.remove(img_path)
+                                # 원본 이미지 삭제
+                                os.remove(img_path)
 
-                            images_converted += 1
-                            self.stdout.write(self.style.SUCCESS(f'Converted {img_path} to {webp_path}'))
+                                images_converted += 1
+                                self.stdout.write(self.style.SUCCESS(f'Converted {img_path} to {webp_path}'))
+                            else:
+                                self.stdout.write(self.style.ERROR(f'Failed to save WebP image: {webp_path}'))
 
                     except Exception as e:
                         self.stdout.write(self.style.ERROR(f'Error processing image {img_path}: {e}'))
 
         self.stdout.write(self.style.SUCCESS(f'Total images converted: {images_converted}'))
-
