6333a27227b6c622cecde54603ea416fd84e19fd
sungkam718 <sungkam718@gmail.com>
2024-12-30 16:32:11 +1100

    updated 265

--- gallery/management/commands/convert_webp.py
+++ gallery/management/commands/convert_webp.py
@@ -8,14 +8,16 @@ from gallery.models import Gallery
 class Command(BaseCommand):
     help = 'Convert the most recent image to WebP format, resize it, and rotate it -90 degrees'
 
-    MAX_WIDTH = 3200
-    MAX_HEIGHT = 3200
+    # 최대 이미지 크기 (너비, 높이)
+    MAX_WIDTH = 3500
+    MAX_HEIGHT = 3500
 
     def handle(self, *args, **kwargs):
         images_converted = 0
 
         self.stdout.write(self.style.SUCCESS('Processing the most recent image...'))
 
+        # 가장 최근 객체 하나 가져오기
         gallery = Gallery.objects.order_by('-id').first()
 
         if not gallery or not gallery.head_image or not gallery.head_image.name:
@@ -33,20 +35,25 @@ class Command(BaseCommand):
                 img_dir, img_filename = os.path.split(img_path)
                 img_name, img_ext = os.path.splitext(img_filename)
 
+                # Apply Exif orientation if exists
                 if img.getexif():
                     img = ImageOps.exif_transpose(img)
                 else:
                     img = img.rotate(-90, expand=True)
 
-                img.thumbnail((self.MAX_WIDTH, self.MAX_HEIGHT), Image.ANTIALIAS)
+                # Resize if image exceeds max dimensions
+                img.thumbnail((self.MAX_WIDTH, self.MAX_HEIGHT), Image.Resampling.NEAREST)
 
+                # Save as WebP
                 webp_path = os.path.join(img_dir, f"{img_name}.webp")
                 img.save(webp_path, 'WEBP')
 
                 if os.path.exists(webp_path):
+                    # Update database record
                     gallery.head_image.name = os.path.relpath(webp_path, settings.MEDIA_ROOT)
                     gallery.save(update_fields=['head_image'])
 
+                    # Remove original image
                     os.remove(img_path)
                     images_converted += 1
 
