238abfa2553fb24155194bbf362c73ea4c4fb460
sungkam718 <sungkam718@gmail.com>
2024-12-30 12:06:08 +1100

    updated 258

--- gallery/management/commands/convert_webp.py
+++ gallery/management/commands/convert_webp.py
@@ -57,7 +57,7 @@ class Command(BaseCommand):
                     webp_path = os.path.join(img_dir, f"{img_name}.webp")
 
                     if img_ext.lower() not in ['.webp']:
-                        img = img.resize((2560, 2560))
+                        img = img.resize((3840, 3840))
                         img.save(webp_path, 'WEBP', quality=80, method=6)
 
                         if os.path.exists(webp_path):
