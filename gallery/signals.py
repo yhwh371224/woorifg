e7a67b30db9a2a149f042abc5d3d60a8458e79cb
sungkam718 <sungkam718@gmail.com>
2024-12-31 12:02:11 +1100

    updated 290

--- gallery/signals.py
+++ gallery/signals.py
@@ -1,20 +1,20 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from .models import Gallery
-from .tasks import convert_webp
-import os
-import logging
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
+# from .models import Gallery
+# from .tasks import convert_webp
+# import os
+# import logging
 
 
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
-@receiver(post_save, sender=Gallery)
-def async_convert_webp(sender, instance, created, **kwargs):
-    if created and instance.head_image:
-        img_path = instance.head_image.name
+# @receiver(post_save, sender=Gallery)
+# def async_convert_webp(sender, instance, created, **kwargs):
+#     if created and instance.head_image:
+#         img_path = instance.head_image.name
 
-        if not img_path.lower().endswith('.webp'):            
-            convert_webp.delay(instance.id)
+#         if not img_path.lower().endswith('.webp'):            
+#             convert_webp.delay(instance.id)
         
-        else:
-            logger.info(f"{img_path} is already in WebP format, no conversion needed.")
+#         else:
+#             logger.info(f"{img_path} is already in WebP format, no conversion needed.")
