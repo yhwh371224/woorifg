3b451c91c9573bf1b8337f860b351bebb84040ae
sungkam718 <sungkam718@gmail.com>
2025-01-02 15:56:48 +1100

    updated 265

--- review/views.py
+++ review/views.py
@@ -198,7 +198,8 @@ class PostSearch(PostList):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['search_info'] = f'Search: "{self.kwargs["q"]}"'
-        context['category'] = self.kwargs.get('category', 'Search Results')
+        self.category = 'Search Results'
+        context['category'] = self.category
         return context
     
 
